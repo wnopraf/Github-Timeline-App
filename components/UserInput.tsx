3fa555a41085ef444a9c5b52ca49940c4ae1806e
wnpraf <rapribu@gmail.com>
2020-01-27 13:15:08 +0100

    Add git to proyect

---
+++ components/UserInput.tsx
@@ -0,0 +1,9 @@
+
+export default (getUserHistory) => {
+
+
+    return <div className="input-box">
+        <input type="text" />
+        <button onClick={getUserHistory}>enviar</button>
+    </div>
+}
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+
+
+const Index = () => {
+    function getUserRequest(params: Event) {
+
+    }
+    const [useUserInput, setUserInput] = useState('')
+}
+
