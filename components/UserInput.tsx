729f4ceefabdd4ed7074448ed4dd5bc7933bcf51
wnpraf <rapribu@gmail.com>
2020-01-28 19:58:30 +0100

    Add props and types to component

--- components/UserInput.tsx
+++ components/UserInput.tsx
@@ -1,9 +1,27 @@
+export default ({
+  setUserName,
+  userName,
+  requestUserRepoData,
+  setRepoData
+}: {
+  setUserName: (String) => void
+  userName: string
+  setRepoData
+  requestUserRepoData: (variables: {
+    userName: string
+    endCursor?: string
+  }) => Promise<any>
+}) => {
+  const clickEvent = async click => {
+    const data = await requestUserRepoData({ userName })
+    console.log(data, 'graph data')
 
-export default ({ setUserName, getUserHistory }: { setUserName: (String) => void, getUserHistory: (Event) => void }) => {
-
-
-    return <div className="input-box">
-        <input type="text" onChange={(e) => setUserName(e.target.value)} />
-        <button onClick={getUserHistory}>enviar</button>
+    setRepoData(data)
+  }
+  return (
+    <div className="input-box">
+      <input type="text" onChange={e => setUserName(e.target.value)} />
+      <button onClick={clickEvent}>enviar</button>
     </div>
+  )
 }
