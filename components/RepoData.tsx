8d508027a24f3be4c8ec5751f22d965ecea2fc6b
wnpraf <rapribu@gmail.com>
2020-02-06 20:26:15 +0100

    Add RepoData component styles

--- components/RepoData.tsx
+++ components/RepoData.tsx
@@ -6,11 +6,16 @@ export default ({ repositories }: { repositories: Repositories }) => {
       {repositories.nodes.map((e, i) => {
         const formattedDate = new Date(e.createdAt).toLocaleDateString()
         return (
-          <li key={i} className="px-3 mt-3 bg-gray-300">
-            <h4>{e.name}</h4>
-            <p>
-              <span>{formattedDate}</span>
-            </p>
+          <li
+            key={i}
+            className="repo-item relative px-3 py-3   border-l-4 border-gray-600"
+          >
+            <div className="pl-4 py-2 bg-blue-600 text-gray-100">
+              <h4>{e.name}</h4>
+              <p>
+                <span>{formattedDate}</span>
+              </p>
+            </div>
           </li>
         )
       })}
